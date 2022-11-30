56e7fd382475bff52616bba8d63534cb794a60f9
Anxhela <118725186+AnxhelaAhmetaj@users.noreply.github.com>
2022-11-30 15:44:31 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,20 +1,21 @@
-import http, { request } from 'http';
+import http from 'http';
 import fsp from 'fs/promises';
 
 const hostname = '127.0.0.2';
 const port = 3000;
 
 const server = http.createServer(async (request, response) => {
-    if(request.url === '/index.html') {
+    console.log(request.url);
+    if(request.url === '/') {
         const content = await fsp.readFile('./index.html', 'utf-8');
         response.statusCode = 200;
-        response.setHeader('Content-Type', 'text/plain');
+        response.setHeader('Content-Type', 'text/html');
         response.end(content); 
     }
-    else if(request.url === '/about.html') {
+    else if(request.url === '/about') {
         const content = await fsp.readFile('./about.html', 'utf-8');
         response.statusCode = 200;
-        response.setHeader('Content-Type', 'text/plain');
+        response.setHeader('Content-Type', 'text/html');
         response.end(content); 
     }
     else {
